c7ff8dec650d3e80a326c8a3c2499fe1b21fb4dc
Hal <zhchaoxing@hotmail.com>
2024-03-19 12:58:17 +0800

    Update api.py

--- modules/api/api.py
+++ modules/api/api.py
@@ -117,15 +117,19 @@ def mask_decode_to_image(encoding):
             # print(f'\nsam/heartbeat: {response.text}\n')
             image = encode_pil_to_base64(image)
             print(f"mask_decode_to_image image mid {type(image)}")
+            dilate_value = 16
             response = requests.post('http://0.0.0.0:8080/sam/heartbeat', json={
                 "input_image": image,
                 "mask": image,
-                "dilate_amount": 16
+                "dilate_amount": dilate_value
             })
+            print(f"mask_decode_to_image response 1 masked_image dilate_amount {len(response.dilate_value)}")
             if "masked_image" in response:
+                print(f"mask_decode_to_image response 2 masked_image length {len(response.masked_image)}")
+                print(f"mask_decode_to_image image mid {type(response.masked_image)}")
                 image = decode_base64_to_image(response.masked_image)
                 print(f"mask_decode_to_image image after {type(image)}")
-                print(f'SAM successfully dilated mask by 16.')
+                print(f'SAM successfully dilated mask by {dilate_value}.')
 
             else:
                 print(f'!!!! Error: SAM did not return a masked_image!')
